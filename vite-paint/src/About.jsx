efd49100531d203de0d7fa331d15460ee362e60e
me <lorraobare@gmail.com>
2025-04-05 21:40:45 +0300

    about updates

--- vite-paint/src/About.jsx
+++ vite-paint/src/About.jsx
@@ -1,11 +1,37 @@
+import "./Styles/About.css";
+import about from './assets/about.jpg';
+
 function About() {
   return (
-    <div className="about">
-      <h1>About Us</h1>
-      <p>We are a company that specializes in providing high-quality services and products.</p>
-      <p>Our team is dedicated to ensuring customer satisfaction and delivering exceptional results.</p>
-      <p>With years of experience in the industry, we have built a reputation for excellence.</p>
-    </div>
+    <section className="about">
+      <div className="about__image">
+      <img src={about} alt="About Us" className="about__img" />
+      </div>
+      <div className="about__content">
+        <h2 className="section__header">We Provide You<br />The Best Experience</h2>
+        <p>
+            At Gemasu, we do not just sell paint or offer painting services—we create an experience. From premium, long-lasting paints to expert craftsmanship, we ensure vibrant, flawless finishes that transform your space. Whether you’re a homeowner, business, or contractor, we provide the best products and services to bring your vision to life. Let us help you create a beautiful, lasting impression.
+        </p>
+        <div className="about__btn">
+          <button className="btn">View Our Work</button>
+        </div>
+      </div>
+      <div className="about__stats">
+        <div>
+          <h4>600+</h4>
+          <p>Homes Transformed</p>
+        </div>
+        <div>
+          <h4>500+</h4>
+          <p>Happy Clients</p>
+        </div>
+        <div>
+          <h4>10+</h4>
+          <p>Years of Experience</p>
+        </div>
+      </div>
+    </section>
+
   );
 }   
 export default About;
