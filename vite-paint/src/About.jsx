ea2c344470ae9ba2ceb78eb80d6cae66284568e1
me <lorraobare@gmail.com>
2025-04-06 13:55:07 +0300

    About updates

--- vite-paint/src/About.jsx
+++ vite-paint/src/About.jsx
@@ -4,13 +4,18 @@ import about from './assets/about.jpg';
 function About() {
   return (
     <section className="about">
-      <div className="about__image">
-      <img src={about} alt="About Us" className="about__img" />
-      </div>
+
+<div
+  className="about__image"
+  style={{
+    backgroundImage: `url(${about})`
+  }}
+>
+</div>
       <div className="about__content">
         <h2 className="section__header">We Provide You<br />The Best Experience</h2>
         <p>
-            At Gemasu, we do not just sell paint or offer painting services—we create an experience. From premium, long-lasting paints to expert craftsmanship, we ensure vibrant, flawless finishes that transform your space. Whether you’re a homeowner, business, or contractor, we provide the best products and services to bring your vision to life. Let us help you create a beautiful, lasting impression.
+            At Gemasu,we do not just sell paint or offer painting services—we create an experience. <br />From premium, long-lasting paints to expert craftsmanship, we ensure vibrant, flawless finishes that transform your space. <br />Whether you’re a homeowner, business, or contractor, we provide the best products and services to bring your vision to life. <br />Let us help you create a beautiful, lasting impression.
         </p>
         <div className="about__btn">
           <button className="btn">View Our Work</button>
