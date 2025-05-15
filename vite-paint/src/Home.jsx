14dee78facd35333ad399f4284134d153e0b9084
me <lorraobare@gmail.com>
2025-05-15 10:59:01 +0300

    home changes

--- vite-paint/src/Home.jsx
+++ vite-paint/src/Home.jsx
@@ -1,49 +1,29 @@
-import { useEffect } from "react";
-import Hero from './assets/Hero.jpg';
-import "./styles/Home.css";
+import { useEffect } from "react"; 
+import Hero from './assets/Hero.jpg'; 
+import "./styles/Home.css";  
 
-function Home() {
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("visible");
-          }
-        });
-      },
-      { threshold: 0.6 }
-    );
-    
-    const elements = document.querySelectorAll(".animate-on-scroll");
-    elements.forEach(el => observer.observe(el));
-    
-    return () => {
-      elements.forEach(el => observer.unobserve(el));
-    };
-  }, []);
+function Home() {   
+  useEffect(() => {const observer = new IntersectionObserver(       
+    (entries) => {   entries.forEach(entry => {           
+    if (entry.isIntersecting) {entry.target.classList.add("visible");}         
+  });       
+  },{ threshold: 0.6 } );const elements = document.querySelectorAll(".animate-on-scroll");     
+  elements.forEach(el => observer.observe(el));      
+  return () => {elements.forEach(el => observer.unobserve(el));};   
+}, 
+[]);    
+  return (     
   
-  return (
-    <header className="section__container header__container" id="home">
-      <div className="header__image">
-        <img src={Hero} alt="hero" />
-      </div>
-      <div className="header__content">
-        <div>
-          <h1 className="hero-title animate-on-scroll">
-            <span className="main-title">Professional Painting Services in Kisumu</span>
-            <span className="brand-name">Gemasu Painters</span>
-          </h1>
-          <p className="hero-description">
-            Gemasu Painters offers expert painting services in Kisumu for homes,
-            offices, and commercial buildings. <br />
-            From premium paint to flawless finishes, we help you transform your
-            space with style and precision.
-          </p>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default Home;
+  <header className="section__container header__container" id="home">      
+   <div className="header__image">         
+    <img src={Hero} alt="hero" />       
+    </div>       
+    <div className="header__content">         
+      <div>         
+        <h1 className="hero__title animate-on-scroll">Professional Painting Services in Kisumu<span className="brand-name">Gemasu Painters</span></h1> 
+        <p className="hero__description">Gemasu Painters offers expert painting services in Kisumu for homes, offices, and commercial buildings. <br />   From premium paint to flawless finishes, we help you transform your   space with style and precision. </p>          
+        </div>       
+        </div>     
+        </header>   
+        );
+}  export default Home;
